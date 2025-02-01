9199aed0e4a093b145f367a8be2c436598b4608f
Aero <aerocyberdev@proton.me>
2025-02-01 20:08:53 +0530

    Add admin and hospitals to the app

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, g
 from staff_api import api
 from admin_api import admin
 from hospital_login import hospital
+from admin_form import admin_routing
+from hospital_form import hospital_routing
 
 app = Flask(__name__)
 app.register_blueprint(api, url_prefix='/api/staff')
 app.register_blueprint(admin, url_prefix='/api/admin')
 app.register_blueprint(hospital, url_prefix='/api/hospital')
+app.register_blueprint(admin_routing, url_prefix='/admin')
+app.register_blueprint(hospital_routing, url_prefix='/hospital')
 
 @app.teardown_appcontext
 def teardown_db(exception):
